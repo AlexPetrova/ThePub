09d331dda11ab1d9357a704254c5bac73bc7f131
AlexPetrova <a.petrova95@yahoo.com>
2020-05-01 17:01:16 +0300

    add AccountController

---
+++ ThePub/ThePub.Application/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ThePub.Application.Models.AccountViewModels;
+using ThePub.Data.DTO;
+using ThePub.Services.Contracts;
+
+namespace ThePub.Application.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserService userService;
+
+        public AccountController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return base.View();
+        }
+
+    }
+}
