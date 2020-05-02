0b045f1996efc38bb4011b34a2901fd84c57397f
AlexPetrova <a.petrova95@yahoo.com>
2020-05-02 11:01:22 +0300

    claim principal extensions

---
+++ ThePub/ThePub.Application/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace ThePub.Application.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static int GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            return int.Parse(claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier));
+        }
+
+        public static string GetUserRole(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindFirstValue(ClaimTypes.Role);
+        }
+    }
+}
